e2e3ae62fbf11ee86232c2d1e0d61192e26c6036
marco301 <marco.rubini08@gmail.com>
2023-07-02 18:48:31 +0200

    improve bitstring parser for visualizing binary trees

--- include/trim/parsing/bitstring.hpp
+++ include/trim/parsing/bitstring.hpp
@@ -12,7 +12,6 @@ namespace trim
       Parse_Result result {};
 
       constexpr std::size_t sentinel = std::size_t(-1);
-      using bit_type = char8_t;
 
       if(text.empty()) {
         return result;
@@ -32,51 +31,47 @@ namespace trim
       }
 
       std::vector<std::size_t> parent_array {};
-      std::vector<bit_type> touched = std::vector<bit_type>(text.size());
+      std::vector<std::size_t> labels {};
 
       std::size_t free_id = 0;
+      std::size_t label_id = 0;
       auto const recurse = [&](auto& rec, std::size_t index) noexcept -> std::size_t {
-        TRIM_ASSERT(index < text.size());
-        TRIM_ASSERT(text[index] == '1');
+        if(index >= text.size() || text[index] == '0') {
+          parent_array.push_back(sentinel);
+          labels.push_back(sentinel);
+
+          std::size_t const curr_id = free_id;
+          free_id += 1;
+          return curr_id;
+        }
 
         std::size_t const left = index * 2 + 1;
         std::size_t const right = index * 2 + 2;
         std::size_t left_id = sentinel;
         std::size_t right_id = sentinel;
 
-        if(left >= text.size()) {
-          // (child does not exist)
-        } else if(text[left] == '0') {
-          // (child does not exist)
-          touched[left] = true;
-        } else if(text[left] == '1') {
+        if(left >= text.size() || text[left] == '0' || text[left] == '1') {
           left_id = rec(rec, left);
         } else {
           // invalid character
           char invalid_character = text[index];
           std::string message = std::string("Invalid character '") + invalid_character + "'";
           result.errors.push_back(Parse_Error(std::move(message), index, 1));
-          touched[left] = true;
         }
 
         parent_array.push_back(sentinel);
+        labels.push_back(label_id);
         std::size_t const curr_id = free_id;
         free_id += 1;
-        touched[index] = true;
+        label_id += 1;
 
-        if(right >= text.size()) {
-          // do nothing (child does not exist)
-        } else if(text[right] == '0') {
-          // do nothing (child does not exist)
-          touched[right] = true;
-        } else if(text[right] == '1') {
+        if(right >= text.size() || text[right] == '0' || text[right] == '1') {
           right_id = rec(rec, right);
         } else {
           // invalid character
           char invalid_character = text[index];
           std::string message = std::string("Invalid character '") + invalid_character + "'";
           result.errors.push_back(Parse_Error(std::move(message), index, 1));
-          touched[right] = true;
         }
 
         if(left_id != sentinel) {
@@ -91,18 +86,6 @@ namespace trim
       };
       std::size_t const root = recurse(recurse, 0);
 
-      std::size_t last_touched = sentinel;
-      for(std::size_t i = 0; i < text.size(); ++i)
-        if(touched[i])
-          last_touched = i;
-
-      for(std::size_t i = 0; i < text.size(); ++i) {
-        if(!touched[i] && i > last_touched) {
-          std::string message = "Trailing characters not consumed.";
-          result.errors.push_back(Parse_Error(std::move(message), i, 1));
-        }
-      }
-
       std::size_t const num_nodes = free_id;
       result.tree = Tree(num_nodes);
       result.node_labels = Labels(num_nodes);
@@ -113,7 +96,12 @@ namespace trim
         if(parent != sentinel) {
           result.tree.add_child(parent, i);
         }
-        result.node_labels[i] = trim::format_integer_to_string(i);
+
+        if(labels[i] == sentinel) {
+          result.node_labels[i] = trim::String("x");
+        } else {
+          result.node_labels[i] = trim::format_integer_to_string(labels[i]);
+        }
         result.edge_labels[i] = trim::String();
       }
 
